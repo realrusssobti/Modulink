069117edd0833a555bdaba865f7e07665823bbc7
Russ Sobti <russ@recombinantsoftware.com>
2024-05-07 20:17:38 -0700

    THE FIRMWARE IS FIRMWARING
    need to add HBridgeDriver.cpp/HBridgeDriver.h next, and set up application layer stuff with @ryanleontini.
    @Nathan904 and I will work on low power modes, RTC waiting, etc and characterize power draw to extrapolate battery life

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -10,106 +10,127 @@
 #include "HardwareSerial.h"
 
 HardwareSerial mySerial {PB7, PB6};
+// LED is on PB5
+#define LED_BUILTIN PB5
+int valve_state = 0; // this is the state of the valve for now (0 = closed, 1 = open)
+
 
 STM32LoRaWAN modem; // get ready to set up the modem
 
 //#include "arduino_secrets.h"
 // Please enter your sensitive data in the Secret tab or arduino_secrets.h
-String appEui = "B1C4E90005012038";
-String appKey = "B1C4E90005012038";
-
+String appEui = "DEADBEEFDEADBEEF";
+String appKey = "BEEFBEEFBEEFBEEFBEEFBEEFBEEFBEEF";
 
 
 // Blinky LED for now
 //#define LED_BUILTIN PB5
 
+void setup() {
 
-void setup(void){
 	// put your setup code here, to run once:
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, LOW);
+
 	mySerial.begin(115200);
-	while (!Serial);
-	// change this toyour regional band (eg. US915, AS923, ...)
+
+	while (!mySerial);
+
+	mySerial.println("Welcome to MKRWAN1300/1310 first configuration sketch");
+
+	mySerial.println("Register to your favourite LoRa network and we are ready to go!");
+
+	// change this to your regional band (eg. US915, AS923, ...)
+
 	if (!modem.begin(US915)) {
+
 		mySerial.println("Failed to start module");
+
 		while (1) {}
+
 	};
+
 	mySerial.print("Your module version is: ");
+
 	mySerial.println(modem.version());
+
 	mySerial.print("Your device EUI is: ");
+
 	mySerial.println(modem.deviceEUI());
 
-	int connected = modem.joinOTAA(appEui, appKey);
+	int connected;
+		appKey.trim();
+
+		appEui.trim();
+		connected = modem.joinOTAA(appEui, appKey);
+
 	if (!connected) {
-		mySerial.println("Something went wrong; are you indoor? Move near a window and retry");
-		// get the last failure reason
-		mySerial.print("Last Error: ");
-		mySerial.println(modem.lastFailure);
-		// print out the appEui and appKey that was used
-		mySerial.print("AppEui: ");
-		mySerial.println(appEui);
-		mySerial.print("AppKey: ");
-		mySerial.println(appKey);
-// stay here
-//		while (1) {}
+
+		mySerial.println("Uh-Oh, Connection failed. Check your keys and distance!");
+
+		while (1) {}
+
 	}
 
-	// Set poll interval to 60 secs.
-	modem.minPollInterval(60);
-	// NOTE: independent of this setting, the modem will
-	// not allow sending more than one message every 2 minutes,
-	// this is enforced by firmware and can not be changed.
-pinMode(PB5, OUTPUT);
-}
-void loop(void){
-	// blink the LED
-	
-
-	mySerial.println();
-	mySerial.println("Enter a message to send to network");
-	mySerial.println("(make sure that end-of-line 'NL' is enabled)");
-
-	while (!mySerial.available());
-	String msg = mySerial.readStringUntil('###');
-
-	mySerial.println();
-	mySerial.print("Sending: " + msg + " - ");
-	for (unsigned int i = 0; i < msg.length(); i++) {
-		mySerial.print(msg[i] >> 4, HEX);
-		mySerial.print(msg[i] & 0xF, HEX);
-		mySerial.print(" ");
-	}
-	mySerial.println();
-
-	int err;
+	mySerial.println("You're connected to the network");
+	String message = "Hello World";
 	modem.beginPacket();
-	modem.print(msg);
-	err = modem.endPacket(true);
+	modem.write((uint8_t*)message.c_str(), message.length());
+	int err = modem.endPacket(true); // true for confirmed packet
+
+
+
+	if (err > 0) {
+
+		mySerial.println("Message sent correctly!");
+
+	} else {
+
+		mySerial.println("Error sending message :(");
+
+	}
+}
+
+void loop() {
+	// Set the builtin LED to the valve state
+	digitalWrite(LED_BUILTIN, !valve_state);
+
+	// Send "open" if the valve state is 1 and "closed" if the valve state is 0
+	String message = valve_state == 1 ? "open" : "closed";
+	modem.beginPacket();
+	modem.write((uint8_t*)message.c_str(), message.length());
+	int err = modem.endPacket(true); // true for confirmed packet
+
 	if (err > 0) {
 		mySerial.println("Message sent correctly!");
 	} else {
 		mySerial.println("Error sending message :(");
-		mySerial.println("(you may send a limited amount of messages per minute, depending on the signal strength");
-		mySerial.println("it may vary from 1 message every couple of seconds to 1 message every minute)");
 	}
-	delay(1000);
-	if (!modem.available()) {
-		mySerial.println("No downlink message received at this time.");
-		return;
-	}
-	char rcv[64];
-	int i = 0;
-	while (modem.available()) {
-		rcv[i++] = (char)modem.read();
-	}
-	mySerial.print("Received: ");
-	for (unsigned int j = 0; j < i; j++) {
-		mySerial.print(rcv[j] >> 4, HEX);
-		mySerial.print(rcv[j] & 0xF, HEX);
-		mySerial.print(" ");
-	}
-	mySerial.println();
 
+	// Read a packet
+	int packetSize = modem.parsePacket();
+	// If there is a packet, read it
+	if (packetSize) {
+		mySerial.print("Received packet: ");
+		// Read the packet into a buffer
+		uint8_t  pkt[packetSize];
+		modem.read(pkt, packetSize);
+		// Convert to char array
+		char packet[packetSize];
+		for (int i = 0; i < packetSize; i++) {
+			packet[i] = (char)pkt[i];
+		}
+		// Print the packet
+		mySerial.println(packet);
+		// check the first part for "open" or "closed"
+		if (strncmp(packet, "open", 4) == 0) {
+			valve_state = 1;
+		} else if (strncmp(packet, "closed", 6) == 0) {
+			valve_state = 0;
+		}
 
+	}
+
+	// Wait 10 seconds
+	delay(10000);
 }
-
-
